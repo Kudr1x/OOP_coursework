9bf1ecdcb99be493634075a0ecb42de3d91ad8c1
laBobberto <chemistgmo@gmail.com>
2024-12-22 03:00:54 +0300

    context error fix

--- src/services/gpt_services.py
+++ src/services/gpt_services.py
@@ -10,16 +10,18 @@ class chatBot():
     def set_context(self, request, context=None):
         if context is not None:
             return context.append({"role": "user", "content": request})
-        return [{"role": "user", "content": request}]
+
+        context = [{"role": "user", "content": request}]
+        return context
 
     def add_to_context(self, context, full_response):
-        context.append({"role": "assistant", "content": full_response.choices[0].message.content})
+        return context.append({"role": "assistant", "content": full_response.choices[0].message.content})
 
     def get_last_resonse(self, full_response):
         return full_response.choices[0].message.content
 
     def get_response(self, request, client, current_model=g4f.models.mistral_large, context=None):
-        self.set_context(request, context)
+        context = self.set_context(request, context)
 
         retries = 3
         for attempt in range(retries):
@@ -28,7 +30,7 @@ class chatBot():
                     model=current_model,
                     messages=context,
                 )
-                self.add_to_context(context, full_response)
+                context = self.add_to_context(context, full_response)
 
                 return self.get_last_resonse(full_response), context
 
